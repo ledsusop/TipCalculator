4d745cdeb9b8ad997a91c8cd23ca04d529900c68
lusop <lusop@walmartlabs.com>
2016-07-09 00:50:08 -0700

    Add Initial Tip Settings View Controller

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
